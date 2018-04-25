f8c9a36977d00d6f622ac1262119e7d6878d73f3
Bruno <brunobar79@gmail.com>
2018-04-25 13:53:25 -0400

    fixes

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const fileUpload = require("express-fileupload");
 
 const app = express();
 
+app.use(fileUpload());
+
 const BASE_SPEECH_API_URL =
   "https://www.naturalreaders.com/api/v4/tts/lgcspeak?apikey=b98x9xlfs54ws4k0wc0o8g4gwc0w8ss&src=pw&s=-1";
 
@@ -13,28 +15,24 @@ app.get("/list", (req, res) => {
   const folder = "./sounds/";
   const files = [];
   fs.readdirSync(folder).forEach(file => {
-    if(file.indexOf("mp3")!==-1){
+    if (file.indexOf("mp3") !== -1) {
       files.push(file);
     }
   });
-   res.send(files.join("\n"));
-  
+  res.send(files.join("\n"));
 });
 
 app.post("/upload", (req, res) => {
-
   if (!req.files) return res.status(400).send("No files were uploaded.");
 
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   let file = req.files.file;
 
   // Use the mv() method to place the file somewhere on your server
-  file.mv("./sounds/"+file.name, function(err) {
+  file.mv("./sounds/" + file.name, function(err) {
     if (err) return res.status(500).send(err);
     res.send("File uploaded!");
   });
-
-
 });
 
 app.get("/", (req, res) => {
@@ -48,7 +46,6 @@ app.get("/", (req, res) => {
     }
 
     let url = BASE_SPEECH_API_URL + "&r=" + speaker + "&t=" + req.query.speak;
-    console.log(url);
     const tmpFilename = "tmp.mp3";
     const file = fs.createWriteStream(tmpFilename);
     const request = https
@@ -60,9 +57,11 @@ app.get("/", (req, res) => {
               .play(tmpFilename)
               .then(r => {
                 res.send(
-                  "Saying " + req.query.speak + (req.query.gender
-                    ? " in a " + req.query.gender + " voice"
-                    : "")
+                  "Saying " +
+                    req.query.speak +
+                    (req.query.gender
+                      ? " in a " + req.query.gender + " voice"
+                      : "")
                 );
               })
               .catch(e => {
